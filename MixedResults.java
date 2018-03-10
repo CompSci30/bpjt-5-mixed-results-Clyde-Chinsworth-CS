539afab1f6e745eb1a21fad43d69a2059701ba4e
Alex Yang <a.yang2@share.epsb.ca>
2018-03-10 20:07:37 +0000

    Declared and intialized problems as variables

--- MixedResults.java
+++ MixedResults.java
@@ -8,5 +8,11 @@ public class MixedResults
 		double d2 = 1004.128;
 		int i1 = 12;
 		int i2 = 18; 
+		
+		double p1 =  57.2 * (i1 / i2) +1;
+		double p2 =  57.2 * ( (double)i1 / i2 ) + 1;
+		double p3 =  15 – i1 * ( d1 * 3) + 4;
+		int p4 =  15 – i1 * (int)( d1 * 3) + 4;
+		int p5 = 15 – i1 * ( (int)d1 * 3) + 4;
 	}
 }
