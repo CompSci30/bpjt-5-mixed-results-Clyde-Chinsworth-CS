65b641e426008570fc40e4fd535096f7346cbedb
Alex Yang <a.yang2@share.epsb.ca>
2018-03-10 20:10:42 +0000

    Added remarks, printed answers

--- MixedResults.java
+++ MixedResults.java
@@ -9,10 +9,16 @@ public class MixedResults
 		int i1 = 12;
 		int i2 = 18; 
 		
-		double p1 =  57.2 * (i1 / i2) +1;
-		double p2 =  57.2 * ( (double)i1 / i2 ) + 1;
-		double p3 =  15 – i1 * ( d1 * 3) + 4;
-		int p4 =  15 – i1 * (int)( d1 * 3) + 4;
-		int p5 = 15 – i1 * ( (int)d1 * 3) + 4;
+		double p1 =  57.2 * (i1 / i2) +1; //1.0
+		double p2 =  57.2 * ( (double)i1 / i2 ) + 1; //39.133333333333
+		double p3 =  15 – i1 * ( d1 * 3) + 4; //-1345.399999999999
+		int p4 =  15 – i1 * (int)( d1 * 3) + 4; //-1337
+		int p5 = 15 – i1 * ( (int)d1 * 3) + 4; //-1313
+		
+		System.out.println("Problem 1: " + p1);
+		System.out.println("Problem 2: " + p2);
+		System.out.println("Problem 3: " + p3);
+		System.out.println("Problem 4: " + p4);
+		System.out.println("Problem 5: " + p5);
 	}
 }
